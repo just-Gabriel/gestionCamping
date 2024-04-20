ef028fcbde2f31f831cb382cb5263cfa5b9b37fa
JustGabriel <maloof123@orange.fr>
2024-04-20 19:21:01 +0200

    V7 il me semble ajout de card pour les logements tout est ok

--- src/Controller/LogementController.php
+++ src/Controller/LogementController.php
@@ -3,10 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+
 
 
 class LogementController extends AbstractController
@@ -21,6 +21,11 @@ class LogementController extends AbstractController
     {
        
        
-        return new Response('type de logement: ' . $slug);
+        return $this->render('logement/show.html.twig', [
+            'slug' => $slug,
+            'id' => $id
+
+        ]);
+        
     }
 }
